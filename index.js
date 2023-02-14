9d4439897ff4b05095d4b95a211174eec1413e0e
KevinCarrillo93 <kevincarrilloacosta@gmail.com>
2023-02-13 21:52:36 -0500

    technical test solution

--- index.js
+++ index.js
@@ -36,8 +36,34 @@ import $t from './libs/test.js'
 import _ from 'lodash'
 const source = $t.source(1)
 $t.answer(1, async () => {
-  // Your code goes here
-  return 
+  // Define an initial object called 'target' with the properties that we're going to use
+  let target = {
+    balance: 0,
+    income: 0,
+    expenses: 0,
+    byCategories:{}    
+  }
+  // Use the 'reduce' function to iterate over the 'source' array and update the 'target' object  
+  return _.reduce(source, (accumulated, n) => {
+    // If the transaction type is 'income', update the 'income' property of the 'target' object
+    if (n.type === 'income') {
+      accumulated.income += n.amount;
+    }
+    // If the transaction type is 'expense', update the 'expenses' property of the 'target' object
+    else if (n.type === 'expense') {
+      accumulated.expenses += n.amount;
+      // If the category property for the transaction doesn't exist yet, initialize it to 0
+      accumulated.byCategories[n.category] = accumulated.byCategories[n.category] || 0;
+      // Then subtract the transaction amount from the corresponding category property in the 'target' object
+      accumulated.byCategories[n.category] -= n.amount;
+    }
+    // Update the 'balance' property of the 'target' object by subtracting 'expenses' from 'income'
+    accumulated.balance = accumulated.income - accumulated.expenses;
+    // Update the 'Income' property of the 'byCategories' property with the current value of 'income'
+    accumulated.byCategories.Income = accumulated.income;
+    // Return the 'accumulated' object for the next iteration of the 'reduce'
+    return accumulated;
+  }, target);
 })
 
 /*
@@ -49,9 +75,17 @@ $t.answer(1, async () => {
 */
 const $source = $t.source(2)
 $t.answer(2, async () => {
-    // Your code goes here:
-    // 1. Get ids: $source.getIds()
-    // 2. Get text for every id: $source.getText(id)
-    // 3. Return array of texts
-    return 
+    try {
+      let array = [];      
+      // 1. Get ids: $source.getIds()
+      let result = await $source.getIds();
+      result.forEach(res => {
+        // 2. Get text for every id: $source.getText(id)
+        array.push($source.getText(res))
+      });
+      // 3. Return array of texts
+      return $source.getAnswer(array)
+    } catch (error) {
+      console.log('Error')
+    }
 })
